ad6df3ae83bdebf04df62400ecee5a31eea76453
Devce87 <sousukehirujo@gmail.com>
2023-02-24 14:58:11 -0500

    test

--- history.js
+++ history.js
@@ -1,11 +1,1 @@
-showTable(){
-    let body = document.createElement('tbody');
-    body.id = 'history-body';
-    for(index=0; index<localStorage.length;index++){
-        const key =  localStorage.key(index);
-        const value =  {store_JSON }= JSON.parse(localStorage.getItem(key));
-    }
-    
-
-
-}
+/*test*/
